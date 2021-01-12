7a8ea0045f2560d482e998d58031e4ace8a3f863
junh0328 <junh0328@naver.com>
2021-01-12 21:08:31 +0900

    페이지, 컴포넌트 만들기

--- ssr-recipe/src/App.js
+++ ssr-recipe/src/App.js
@@ -1,5 +1,18 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+import Menu from './components/Menu';
+import RedPage from './components/Red';
+import BluePage from './components/Blue';
+
 function App() {
-  return <div>Hello React!</div>;
+  return (
+    <div>
+      <Menu />
+      <ht />
+      <Route path='/red' component={RedPage} />
+      <Route path='/blue' component={BluePage} />
+    </div>
+  );
 }
 
 export default App;
